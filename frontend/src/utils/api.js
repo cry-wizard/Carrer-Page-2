7f39d51120ba8179dfc8398d16f64219c1f01cbb
cry-wizard <sidd160306@gmail.com>
2026-03-10 20:11:37 +0530

    Adding backend

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://carrer-page-2.onrender.com/api',
 });
 
 // Request interceptor to add the auth token header to requests
